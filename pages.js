cbe0f90c48acc0e9aa55265a4f85043ef360bebe
OE-Heart <leoouyixin@126.com>
2024-10-23 16:03:36 +0800

    Update

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/Tracey-Li-MUSK/Tracy-Li-MUSK.github.io.git";
+const repoURL = "https://github.com/Tracey-Li-MUSK/Tracey-Li-MUSK.github.io.git";
 
 ghpages.publish(
   pathname,
